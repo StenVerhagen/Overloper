cd98578a577648b0fe768a4580a914674f7197d8
DaanvanDael <113184232+DaanvanDael@users.noreply.github.com>
2023-10-26 14:02:27 +0000

    Ik heb de bommen en de tekst linksbovenin toegevoegd

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
+var  playerleven = 5;
+
+class Bom {
+  constructor() {
+    this.x = floor(random(9,raster.aantalKolommen))*raster.celGrootte;
+    this.y = floor(random(1,12,raster.aantalRijen))*raster.celGrootte;
+    this.snelheid =  5;
+    this.yRichting = 5;
+  }
+  
+  beweeg() {
+    this.y -= this.snelheid * this.yRichting;
+
+    if (this.y >= canvas.height - raster.celGrootte || this.y <= 0) {
+      this.yRichting *= -1;
+    }
+    this.x = constrain(this.x, 0, canvas.width - raster.celGrootte);
+  }
+  
+  toon() {
+    image(bomPlaatje,this.x,this.y,raster.celGrootte,raster.celGrootte);
+  }
+}
+
 class Raster {
   constructor(r,k) {
     this.aantalRijen = r;
     this.aantalKolommen = k;
     this.celGrootte = null;
+    this.oranjeRegel = r;
   }
   
   berekenCelGrootte() {
@@ -12,10 +37,15 @@ class Raster {
   teken() {
     push();
     noFill();
-    stroke('grey');
+    stroke('black');
     for (var rij = 0;rij < this.aantalRijen;rij++) {
       for (var kolom = 0;kolom < this.aantalKolommen;kolom++) {
-        rect(kolom*this.celGrootte,rij*this.celGrootte,this.celGrootte,this.celGrootte);
+        if (rij === this.oranjeRegel - 1 || kolom === this.oranjeRegel -12) {
+          fill ('orange');
+        } else(
+          noFill()
+        )
+          rect(kolom*this.celGrootte,rij*this.celGrootte,this.celGrootte,this.celGrootte);
       }
     }
     pop();
@@ -24,7 +54,7 @@ class Raster {
 
 class Jos {
   constructor() {
-    this.x = 400;
+    this.x = 0;
     this.y = 300;
     this.animatie = [];
     this.frameNummer =  3;
@@ -66,11 +96,38 @@ class Jos {
       return false;
     }
   }
+
+staatOp(bommenLijst) {
+    for (var b = 0;b < bommenLijst.length;b++) {
+      if (bommenLijst[b].x == this.x && bommenLijst[b].y == this.y) {
+        this.staOpBom = true;
+      }
+    }
+    return this.staOpBom;
+  } 
   
   toon() {
     image(this.animatie[this.frameNummer],this.x,this.y,raster.celGrootte,raster.celGrootte);
   }
-}  
+}
+
+
+class ExtraLife {
+  constructor() {
+    this.x = random(canvas.width - raster.celGrootte);
+    this.y = random(canvas.height - raster.celGrootte);
+  }
+
+  toon() {
+    if (!this.collected) {
+      image(this.sprite, this.x, this.y, raster.celGrootte, raster.celGrootte);
+    }
+  }
+}
+
+let extraLife;
+
+
 
 class Vijand {
   constructor(x,y) {
@@ -87,43 +144,52 @@ class Vijand {
     this.x = constrain(this.x,0,canvas.width - raster.celGrootte);
     this.y = constrain(this.y,0,canvas.height - raster.celGrootte);
   }
-  
-  toon() {
-    image(this.sprite,this.x,this.y,raster.celGrootte,raster.celGrootte);
+
+toon(){
+  image(this.sprite,this.x,this.y,raster.celGrootte,raster.celGrootte);
   }
 }
 
 function preload() {
-  brug = loadImage("images/backgrounds/dame_op_brug_1800.jpg");
+  brug = loadImage("images/backgrounds/skyline_1.jpg");
+  bomPlaatje = loadImage("images/sprites/bom_100px.png");
 }
+
+var bommenArray = [];
 
 function setup() {
   canvas = createCanvas(900,600);
   canvas.parent();
   frameRate(10);
-  textFont("Verdana");
+  textFont("Georgia");
   textSize(90);
   
   raster = new Raster(12,18);
   
   raster.berekenCelGrootte();
+  bomba1 = new Bom();
+  bomba2 = new Bom();
+  bomba3 = new Bom();
+  bomba4 = new Bom();
+  bomba5 = new Bom();
   
   eve = new Jos();
   eve.stapGrootte = 1*raster.celGrootte;
   for (var b = 0;b < 6;b++) {
-    frameEve = loadImage("images/sprites/Eve100px/Eve_" + b + ".png");
+    frameEve = loadImage("images/sprites/roodborstje_R.png");
     eve.animatie.push(frameEve);
   }
-  
+
+  extraLife = new ExtraLife();
+  extraLife.sprite = loadImage("images/sprites/appel_2.png");
+
   alice = new Vijand(700,200);
   alice.stapGrootte = 1*eve.stapGrootte;
   alice.sprite = loadImage("images/sprites/Alice100px/Alice.png");
 
   bob = new Vijand(600,400);
   bob.stapGrootte = 1*eve.stapGrootte;
-  bob.sprite = loadImage("images/sprites/Bob100px/Bob.png"); 
-
-
+  bob.sprite = loadImage("images/sprites/Bob100px/Bob.png");  
 }
 
 function draw() {
@@ -132,18 +198,47 @@ function draw() {
   eve.beweeg();
   alice.beweeg();
   bob.beweeg();
+  bomba1.beweeg();
+  bomba2.beweeg();
+  bomba3.beweeg();
+  bomba4.beweeg();
+  bomba5.beweeg();
   eve.toon();
   alice.toon();
   bob.toon();
+  extraLife.toon();
+  bomba1.toon();
+  bomba2.toon();
+  bomba3.toon();
+  bomba4.toon();
+  bomba5.toon();
+
+  fill ('white');
+  textSize(50);
+  text("Levens:" + playerleven, 50, 50);
   
-  if (eve.wordtGeraakt(alice) || eve.wordtGeraakt(bob)) {
-    noLoop();
+  if (eve.wordtGeraakt(alice) || eve.wordtGeraakt(bob) || eve.wordtGeraakt(bomba1) || eve.wordtGeraakt(bomba2) || eve.wordtGeraakt(bomba3) || eve.wordtGeraakt(bomba4) || eve.wordtGeraakt(bomba5)) {
+    playerleven -= 1;
+if (playerleven <= 0){
+  
+    
+    background("red")
+    fill ("black")    
+    textSize(250);
+    text ("Loser!!!",10,375)
+    noloop();
+  }
   }
   
+ if (!extraLife.collected && dist(eve.x, eve.y, extraLife.x, extraLife.y) < raster.celGrootte) {
+   extraLife.collected = true;
+  }
+
   if (eve.gehaald) {
     background('green');
     fill('white');
-    text("Je hebt gewonnen!",30,300);
+    textSize(95);
+    text("Bartje voor de winst!",10,340);
     noLoop();
   }
 }
